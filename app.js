efbdd786437639e23d6203e4a2133233869ba050
Sahana Jaloji <sahanajalojigit@gmail.com>
2025-09-07 12:19:09 +0530

    home route changed

--- app.js
+++ app.js
@@ -17,6 +17,7 @@ const flash = require("connect-flash");
 const passport = require("passport");
 const LocalStrategy = require("passport-local");
 const User = require("./models/user.js");
+const router = express.Router();
 
 const listingRouter = require("./routes/listing.js");
 const reviewRouter = require("./routes/review.js");
@@ -51,7 +52,7 @@ const sessionOptions = {
     }
 }
 
-app.get("/", (req, res) => {
+router.get("/", (req, res) => {
     res.render("listings/index.ejs");
 })
 
