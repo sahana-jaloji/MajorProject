06910fa05fde0295aa9bc9c27e77346b3663da11
Sahana Jaloji <sahanajalojigit@gmail.com>
2025-09-07 11:27:20 +0530

    home route

--- app.js
+++ app.js
@@ -51,9 +51,9 @@ const sessionOptions = {
     }
 }
 
-app.get("/", (req, res) => {
-    res.send("working..");
-})
+// app.get("/", (req, res) => {
+//     res.send("working..");
+// })
 
 app.use(session(sessionOptions));
 app.use(flash());
